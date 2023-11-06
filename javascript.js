707e38e5492660b505dccca926608d0154c4ece3
JT <143206481+JohnTHancock@users.noreply.github.com>
2023-11-06 15:12:56 -0500

    Add javascript logic for math
    Added all the javascript wiring for the numbers to evaluate properly.
    Still running into an issue where when a solution is presented and
    a new number is pressed, the new number simply appends to the solution
    instead of starting over.

---
+++ javascript.js
@@ -0,0 +1,122 @@
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
+let shouldResetScreen = false;
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const equalsButton = document.getElementById('equalsBtn');
+const clearButton = document.getElementById('clearBtn');
+const deleteButton = document.getElementById('deleteBtn');
+const decimalButton = document.getElementById('decimalBtn');
+const lastOperationScreen = document.getElementById('screenLast');
+const currentOperationScreen = document.getElementById('screenCurrent');
+
+equalsButton.addEventListener('click', calculate)
+clearButton.addEventListener('click', clear)
+deleteButton.addEventListener('click', deleteNumber)
+decimalButton.addEventListener('click', appendDecimal)
+
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operatorButtons.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+function addition(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+function resetScreen() {
+    currentOperationScreen.textContent = '';
+    shouldResetScreen = false;
+}
+
+function appendNumber(number) {
+    if (currentOperationScreen.textContent === '0' || shouldResetScreen) resetScreen();
+    currentOperationScreen.textContent += number;
+}
+
+function setOperation(operator) {
+    if (currentOperation !== null) calculate()
+    firstOperand = currentOperationScreen.textContent
+    currentOperation = operator
+    lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`
+    shouldResetScreen = true
+}
+
+function calculate() {
+    if (currentOperation === null || shouldResetScreen) return
+    if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
+        alert('go fuck yourself');
+        return;
+    }
+    secondOperand = currentOperationScreen.textContent;
+    currentOperationScreen.textContent = roundResult(operate(currentOperation, firstOperand, secondOperand));
+    lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+    currentOperation = null;
+}
+
+function deleteNumber() {
+    currentOperationScreen.textContent = currentOperationScreen.textContent
+      .toString()
+      .slice(0, -1)
+  }
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000
+  }
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return addition(a, b);
+        case '-':
+            return subtraction(a, b);
+        case 'x':
+            return multiplication(a, b);
+        case '÷':
+            if (b === 0) return null;
+            else return division(a, b);
+        default:
+            return null
+    }
+}
+
+function clear() {
+    currentOperationScreen.textContent = '0'
+    lastOperationScreen.textContent = ''
+    firstOperand = ''
+    secondOperand = ''
+    currentOperation = null
+  }
+  
+  function appendDecimal() {
+    if (shouldResetScreen) resetScreen()
+    if (currentOperationScreen.textContent === '')
+      currentOperationScreen.textContent = '0'
+    if (currentOperationScreen.textContent.includes('.')) return
+    currentOperationScreen.textContent += '.'
+  }
+  
+  function deleteNumber() {
+    currentOperationScreen.textContent = currentOperationScreen.textContent
+      .toString()
+      .slice(0, -1)
+  }
